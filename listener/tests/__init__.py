12d8a210ef2173133371dc05f3562ddd7ba87f6e
yurij <stiker757@mail.ru>
2022-08-09 14:04:15 +0300

    add entity chat

--- listener/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-
-def test_ping(client):
-    response = client.get("/ping")
-    assert response.status_code == 200
-    assert response.json() == {"ping": "pong!"}
--- listener/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import fastapi
-import pytest
-from starlette.testclient import TestClient
-from app import app
-
-
-@pytest.fixture(scope="session")
-def client():
-    yield TestClient(app)
